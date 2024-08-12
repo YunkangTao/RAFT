44b0eee23b4dd409ae502df3fea91fb70e5ed76b
Yunkang Tao <1461932145@qq.com>
2024-08-12 16:50:49 +0800

    update demo.py

--- demo.py
+++ demo.py
@@ -98,7 +98,7 @@ def save_flow_video(video_path, flow_list, height, width, frames):
     video_writer.release()
 
 
-def demo_from_video(args):
+def demo_from_videos(args):
     model = torch.nn.DataParallel(RAFT(args))
     model.load_state_dict(torch.load(args.model))
 
@@ -106,38 +106,42 @@ def demo_from_video(args):
     model.to(DEVICE)
     model.eval()
 
-    video_path = args.path
-    frames, height, width = read_video(video_path)
+    videos_path = glob.glob(os.path.join(args.videos_direction_path, '*.mp4'))
 
-    flow_low_list = []
-    flow_up_list = []
+    for video_path in videos_path:
+        frames, height, width = read_video(video_path)
 
-    with torch.no_grad():
-        for i in range(len(frames) - 1):
-            image1 = cvt_to_tensor(frames[i])
-            image2 = cvt_to_tensor(frames[i + 1])
+        flow_low_list = []
+        flow_up_list = []
 
-            padder = InputPadder(image1.shape)
-            image1, image2 = padder.pad(image1, image2)
+        with torch.no_grad():
+            for i in range(len(frames) - 1):
+                image1 = cvt_to_tensor(frames[i])
+                image2 = cvt_to_tensor(frames[i + 1])
 
-            flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            flow_up_list.append(flow_up)
-            flow_low_list.append(flow_low)
-            # viz(image1, flow_up)
+                padder = InputPadder(image1.shape)
+                image1, image2 = padder.pad(image1, image2)
 
-    save_flow_up_path = os.path.join(os.path.dirname(video_path), 'flow_up.mp4')
-    save_flow_down_path = os.path.join(os.path.dirname(video_path), 'flow_down.mp4')
-    save_flow_video(save_flow_up_path, flow_up_list, height, width, frames)
-    save_flow_video(save_flow_down_path, flow_low_list, height, width, frames)
+                flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
+                flow_up_list.append(flow_up)
+                flow_low_list.append(flow_low)
+                # viz(image1, flow_up)
+
+        # join the path of video name and save flow video
+        save_flow_up_path = os.path.join(args.results_direction_path, video_path.split('/')[-1], 'flow_up.mp4')
+        save_flow_down_path = os.path.join(os.path.dirname(video_path.split('/')[-1]), 'flow_down.mp4')
+        save_flow_video(save_flow_up_path, flow_up_list, height, width, frames)
+        save_flow_video(save_flow_down_path, flow_low_list, height, width, frames)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default="models/raft-things.pth", help="restore checkpoint")
-    parser.add_argument('--path', default="assets/10041038.mp4", help="dataset for evaluation")
+    parser.add_argument('--videos_direction_path', default="assets", help="dataset for evaluation")
+    parser.add_argument('--results_direction_path', default="results", help="directory to save results")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
     args = parser.parse_args()
 
-    demo_from_video(args)
+    demo_from_videos(args)
